7d4db9749c959355ce5f6346958cea50b484ab93
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-04-12 01:24:29 -0400

    Update report1.sh

--- report1.sh
+++ report1.sh
@@ -19,66 +19,71 @@
 #               - Indicator of whether the web server is up and running
 
 
+
 echo "" # Blank line for readability
 
-#username
-userName=$(whoami) 
-# Could alternatively use: userName='whoami'
 
+
+#    Username Field
+userName=$(whoami) #whoami displays the account you are currently logged into
 echo "Information for user: $userName"
 echo ""
-#username
 
 
-#fullname
+
+#    Full name Field
 userEntryData=$(cat /etc/passwd | grep $userName)
-#username:pass:UID:GID:Fullname:homeDir:shell
-#   1    :  2 : 3 : 4 :   5    :   6   :  7
 
-fullName=$(echo "$userEntryData" | cut -d ':' -f 5)
+#          username:pass:UID:GID:Fullname:homeDir:shell
+#             1    :  2 : 3 : 4 :   5    :   6   :  7
+
+fullName=$(echo "$userEntryData" | cut -d ':' -f 5) # -f 5 because we want 5th column of data as shown above
 echo "Full name: $fullName"
 echo ""
-#fullname
 
 
-#home dir
+
+#    Home directory Field
 userEntryData=$(cat /etc/passwd | grep $userName)
-#username:pass:UID:GID:Fullname:homeDir:shell
-#   1    :  2 : 3 : 4 :   5    :   6   :  7
 
-homeDir=$(echo "$userEntryData" | cut -d ':' -f 6)
+#          username:pass:UID:GID:Fullname:homeDir:shell
+#             1    :  2 : 3 : 4 :   5    :   6   :  7
+
+homeDir=$(echo "$userEntryData" | cut -d ':' -f 6) # -f 6 because we want 6th column of data as shown above
 echo "Home directory: $homeDir"
 echo ""
-#home dir
 
 
-#Shell
+
+#    Shell Field
 userEntryData=$(cat /etc/passwd | grep $userName)
-#username:pass:UID:GID:Fullname:homeDir:shell
-#   1    :  2 : 3 : 4 :   5    :   6   :  7
 
-shell=$(echo "$userEntryData" | cut -d ':' -f 7)
+#          username:pass:UID:GID:Fullname:homeDir:shell
+#             1    :  2 : 3 : 4 :   5    :   6   :  7
+
+shell=$(echo "$userEntryData" | cut -d ':' -f 7) # -f 7 because we want 7th column of data as shown above
 echo "Shell: $shell"
 echo ""
-#Shell
 
 
-#password expiration
+
+#    Password expiration Field
 passwordExpirationLine=$(chage -l $userName | grep "Password expires")
-#Password expires                                        : never
 
-passwordExpirationDate=$(echo "$passwordExpirationLine" | cut -d ':' -f 2)
+#           Password expires                                        : never
+
+passwordExpirationDate=$(echo "$passwordExpirationLine" | cut -d ':' -f 2) # -f because we want 2nd column of data as shown above
 echo "Password expires: $passwordExpirationDate"
 echo ""
-#password expiration
 
 
-#Last logins for user
+
+#    Last login attempts for user Field
 numberLastLogins=10
 
 if [ $# -eq 1 ]
 then
-   numberLastLogins=$1
+   numberLastLogins=$1 # setting numberLastLogins to parameter provided, if a parameter is provided
 fi
 
 echo "Last $numberLastLogins logins for $userName:"
@@ -86,36 +91,31 @@ loginData=$(last -$numberLastLogins $userName)
 echo "$loginData"
 echo ""
 #maybe do not include the wtmp data section. | grep -v "wtmp begins" at end of 85
-##OpenLast logins for user
 
 
-#Uptime and users on this system
+
+#    Uptime and users on this system Field
 echo "Uptime and users on this system:" 
 echo $(w)
 echo ""
-#Uptime and users on this system
 
 
-#Open TCP ports
+
+#    Open TCP ports Field
 #output open tcp ports
 echo "Open TCP ports:"
 tcpOpenPortData=$(netstat -tan)
 echo "$tcpOpenPortData"
-
 echo ""
-#Open TCP ports
 
 
 
-#webserver up
-webServerStatusLines=$(ps aux | grep httpd)
-# USER | PID | %CPU | %MEM | VSZ | RSS | TTY | STAT | START | TIME | COMMAND
-# root |  1  |  0.0 |  0.0 |166652|12000| ?  |  Ss  | Apr11 | 0:01 | /sbin/init splash
+#    Web server up Field
+webServerStatusLines=$(ps aux | grep httpd) # grep'ing lines with httpd
 
-if echo "webServerStatusLines" | grep -q 'root'; then
+if echo "webServerStatusLines" | grep -q 'root'; then  # grep'ing again to see if there exists a subset of lines with root
     echo "The web server is running."
 else
     echo "The web server is not running."
 fi
 echo ""
-#webserver up
